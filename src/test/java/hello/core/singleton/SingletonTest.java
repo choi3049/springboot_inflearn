9b3aa31261a6fdece20c0e79aecc31dfe8c423aa
CHOE BYEONGSEON <qudtjs3012@naver.com>
2022-11-29 22:51:47 +0900

    5.1 Web Applications and Single Ton

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,29 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+    @Test
+    @DisplayName("springがない純粋なDIコンテナ")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        //1. 照会：呼び出すたびにオブジェクトを生成
+        MemberService memberService1 = appConfig.memberService();
+
+        //2. 照会：呼び出すたびにオブジェクトを生成
+        MemberService memberService2 = appConfig.memberService();
+
+        //参照値が異なることを確認
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService != memberService2
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+
+    }
+}
